988e56c4998f52f69b40a67bfc44d1b4029b5b15
Robert <robert.3dgamedev@gmail.com>
2023-12-21 20:09:33 +0100

    Refactor error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,29 @@
-extern crate walkdir;
+#![allow(non_camel_case_types)]
 
-pub fn all_files_in_dir<P: AsRef<std::path::Path>>(path: P) -> Vec<std::path::PathBuf>
+extern crate walkdir;
+#[macro_use] extern crate thiserror;
+
+pub fn all_files_in_dir<P: AsRef<std::path::Path>>(path: P) -> Result<Vec<std::path::PathBuf>, Error>
 {
   use crate::walkdir::WalkDir;
   
   let mut paths = vec![];
   for entry in WalkDir::new(path)
   {
-    let entry = entry.unwrap();
+    let entry = entry?;
     paths.push(entry.into_path());
   }
 
-  paths
+  Ok(paths)
+}
+
+pub type Result<T=(), E=Error> = std::result::Result<T, E>;
+
+#[derive(Debug, Error)]
+pub enum Error
+{
+  #[error("io-error: {0}")]
+  IO(#[from] std::io::Error),
+  #[error("io-error: {0}")]
+  FILE_WALKER_ERROR(#[from] walkdir::Error),
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 extern crate c_biber;
 
-fn main() -> std::io::Result<()>
+fn main() -> c_biber::Result
 {
   let curr_dir = std::env::current_dir()?;
-  dbg!(c_biber::all_files_in_dir(curr_dir));
+  dbg!(c_biber::all_files_in_dir(curr_dir)?);
 
   Ok(())
 }
