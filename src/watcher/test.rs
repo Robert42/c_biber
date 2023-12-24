6179065961d947b945deb29b918f581ebbbfbc3d
Robert <robert.3dgamedev@gmail.com>
2023-12-24 10:13:39 +0100

    Refactor

--- src/watcher/test.rs
+++ src/watcher/test.rs
@@ -8,9 +8,10 @@ fn find_all_c_files()
   assert_eq!(create_and_find_files(["a.c", "b.c", "c.rs"]).unwrap(), vec!["a.c", "b.c"]);
 }
 
-fn create_files<'a, Files>(files: Files) -> Result<TempDir>
+fn create_files<'a, Files, Content>(files: Files) -> Result<TempDir>
 where
-  Files: IntoIterator<Item=(&'a str, &'a [u8])>
+  Files: IntoIterator<Item=(&'a str, Content)>,
+  Content: AsRef<[u8]>,
 {
   let tmp_dir = TempDir::new("find_all_c_files").unwrap();
 
@@ -32,7 +33,7 @@ where
   let root = tmp_dir.path();
 
   let mut files = vec![];
-  let receiver = watch(root, |p| Some(p.extension()?=="c"))?;
+  let receiver = watch(root, is_c_file)?;
   while let Ok(event) = receiver.recv()
   {
     use watcher::Watch_Event::*;
@@ -61,4 +62,9 @@ where
   let mut files = collect_cache(files.into_iter().map(|f| (f, nothing)))?;
   files.sort();
   Ok(files)
+}
+
+fn is_c_file(path: &Path) -> Option<bool>
+{
+  Some(path.extension()?=="c")
 }
