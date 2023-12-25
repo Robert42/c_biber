473b09f3f4f4cdcf4e98c2ded0eac9b879468fde
Robert <robert.3dgamedev@gmail.com>
2023-12-25 22:49:34 +0100

    Keep it simple

--- src/tool_finder.rs
+++ src/tool_finder.rs
@@ -8,33 +8,11 @@ pub enum Compiler
 
 pub fn find_compiler() -> Result<Option<Compiler>>
 {
-  let path = std::env::var("PATH")?;
-  let sep = ':'; // TODO: Add Windows support
-  let paths = path.split(sep);
-  for read_dir in paths.filter_map(|path| fs::read_dir(path).ok())
+  if let Ok(output) = std::process::Command::new("gcc").arg("--version").output()
   {
-    for path in read_dir
-      .filter_map(|e| e.ok())
-      .filter_map(|e| if e.file_type().ok()?.is_file() {return Some(e)} else {Some(e)} )
-      .filter_map(|e|
-        {
-          let t = e.file_type().ok()?;
-          if t.is_file() || t.is_symlink() { return Some(e.path()) };
-          None
-        })
+    if output.status.success()
     {
-      let file_name = if let Some(f) = path.file_name() {f} else {continue};
-      if file_name == "gcc"
-      {
-        if let Ok(output) = std::process::Command::new("gcc").arg("--version").output()
-        {
-          if output.status.success()
-          {
-            return Ok(Some(Compiler::GCC(path)));
-          }
-        }
-      }
-
+      return Ok(Some(Compiler::GCC(PathBuf::from("gcc"))));
     }
   }
   Ok(None)
